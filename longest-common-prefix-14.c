9ed7082d64bd5f95554b81cc166cc609c62641a5
owen-rote <rotenbergowen@csu.fullerton.edu>
2024-11-18 00:03:24 -0800

    format

--- longest-common-prefix-14.c
+++ longest-common-prefix-14.c
@@ -7,24 +7,24 @@ Space Complexity: O(n) (zero additional space is used other than result)
 */
 
 char* longestCommonPrefix(char** strs, int strsSize) {
-    int len = strlen(strs[0]);
-    // Allocate new string
-    char* result = (char*)malloc(len + 1);
-    result[0] = '\0';
+  int len = strlen(strs[0]);
+  // Allocate new string
+  char* result = (char*)malloc(len + 1);
+  result[0] = '\0';
 
-    // For each character of current string
-    for (int i = 0; i < len; ++i) {
-        // For each string in strs
-        for (int j = 0; j < strsSize; ++j) {
-            // i: current char; j: current string
-            if (result[i] == '\0') {
-                result[i] = strs[j][i];
-                result[i + 1] = '\0';
-            } else if (strs[j][i] != result[i]) {
-                result[i] = '\0';
-                return result;
-            }
-        }
+  // For each character of current string
+  for (int i = 0; i < len; ++i) {
+    // For each string in strs
+    for (int j = 0; j < strsSize; ++j) {
+      // i: current char; j: current string
+      if (result[i] == '\0') {
+        result[i] = strs[j][i];
+        result[i + 1] = '\0';
+      } else if (strs[j][i] != result[i]) {
+        result[i] = '\0';
+        return result;
+      }
     }
-    return result;
+  }
+  return result;
 }
