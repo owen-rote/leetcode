887b00176d4da2c6b016ddf7d0560d23b625ba8a
owen-rote <rotenbergowen@gmail.com>
2025-09-16 23:51:21 -0700

    format

--- reverse-linked-list-206.c
+++ reverse-linked-list-206.c
@@ -9,37 +9,37 @@ Space Complexity: O(1)
 #include <stdio.h>
 
 struct ListNode {
-  int val;
-  struct ListNode* next;
+    int val;
+    struct ListNode* next;
 };
 
 struct ListNode* reverseList(struct ListNode* head) {
-  if (head == NULL || head->next == NULL) {
-    return head;
-  }
-  struct ListNode* left = head;
-  struct ListNode* right = head;
-
-  while (right != NULL) {
-    if (left == right) {
-      right = right->next;
-      left->next = NULL;
-      head = right;
-      continue;
+    if (head == NULL || head->next == NULL) {
+        return head;
     }
-    // Go through forward and switch the pointer to previous
-    struct ListNode* temp = right->next;
-    right->next = left;
-    left = right;
-    right = temp;
-    head = right != NULL ? right : head;
-  }
+    struct ListNode* left = head;
+    struct ListNode* right = head;
 
-  // DEBUG: Print list
-  // struct ListNode* it = head;
-  // while (it != NULL) {
-  //     printf("%d ", it->val);
-  //     it = it->next;
-  // }
-  return head;
+    while (right != NULL) {
+        if (left == right) {
+            right = right->next;
+            left->next = NULL;
+            head = right;
+            continue;
+        }
+        // Go through forward and switch the pointer to previous
+        struct ListNode* temp = right->next;
+        right->next = left;
+        left = right;
+        right = temp;
+        head = right != NULL ? right : head;
+    }
+
+    // DEBUG: Print list
+    // struct ListNode* it = head;
+    // while (it != NULL) {
+    //     printf("%d ", it->val);
+    //     it = it->next;
+    // }
+    return head;
 }
