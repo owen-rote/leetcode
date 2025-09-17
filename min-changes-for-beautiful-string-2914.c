8bc42dda40fe87a4ed27c76de515f14968fb0ba7
owen-rote <rotenbergowen@gmail.com>
2025-09-16 23:51:31 -0700

    format

--- min-changes-for-beautiful-string-2914.c
+++ min-changes-for-beautiful-string-2914.c
@@ -9,15 +9,15 @@ Space Complexity: O(1)
 #include <string.h>
 
 int minChanges(char* s) {
-  int min_changes = 0;
-  int length = strlen(s);
+    int min_changes = 0;
+    int length = strlen(s);
 
-  // Assumes partitions of exactly two.
-  // If nums in partition aren't equal, min_changes++;
-  for (int i = 0; i < length; i += 2) {
-    if (s[i] != s[i + 1]) {
-      ++min_changes;
+    // Assumes partitions of exactly two.
+    // If nums in partition aren't equal, min_changes++;
+    for (int i = 0; i < length; i += 2) {
+        if (s[i] != s[i + 1]) {
+            ++min_changes;
+        }
     }
-  }
-  return min_changes;
+    return min_changes;
 }
